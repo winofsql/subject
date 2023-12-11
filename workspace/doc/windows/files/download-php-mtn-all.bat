e919d8b6f9d1a430fc0530f36aa200522d495b64
sworc <lightbox@nifty.com>
2023-12-11 14:29:44 +0900

    Add files via upload

---
+++ workspace/doc/windows/files/download-php-mtn-all.bat
@@ -0,0 +1,15 @@
+git clone https://github.com/winofsql/php-mtn-v01-update-sqlite.git && cd php-mtn-v01-update-sqlite && rmdir .git /S /Q && cd ..
+git clone https://github.com/winofsql/php-mtn-v02-update-model-sqlite.git && cd php-mtn-v02-update-model-sqlite && rmdir .git /S /Q && cd ..
+git clone https://github.com/winofsql/php-mtn-v03-update-insert-sqlite.git && cd php-mtn-v03-update-insert-sqlite && rmdir .git /S /Q && cd ..
+git clone https://github.com/winofsql/php-mtn-v04-communication-sqlite.git && cd php-mtn-v04-communication-sqlite && rmdir .git /S /Q && cd ..
+git clone https://github.com/winofsql/php-mtn-v05-enter-control-sqlite.git && cd php-mtn-v05-enter-control-sqlite && rmdir .git /S /Q && cd ..
+git clone https://github.com/winofsql/php-mtn-v06-combo-box-sqlite.git && cd php-mtn-v06-combo-box-sqlite && rmdir .git /S /Q && cd ..
+git clone https://github.com/winofsql/php-mtn-v07-html-validator-sqlite.git && cd php-mtn-v07-html-validator-sqlite && rmdir .git /S /Q && cd ..
+git clone https://github.com/winofsql/php-mtn-v08-sql-adjustment-sqlite.git && cd php-mtn-v08-sql-adjustment-sqlite && rmdir .git /S /Q && cd ..
+git clone https://github.com/winofsql/php-mtn-v09-type-date-sqlite.git && cd php-mtn-v09-type-date-sqlite && rmdir .git /S /Q && cd ..
+git clone https://github.com/winofsql/php-mtn-v10-check-entry-sqlite.git && cd php-mtn-v10-check-entry-sqlite && rmdir .git /S /Q && cd ..
+git clone https://github.com/winofsql/php-mtn-v11-edit-control-sqlite.git && cd php-mtn-v11-edit-control-sqlite && rmdir .git /S /Q && cd ..
+git clone https://github.com/winofsql/php-mtn-v12-iframe-ref1-sqlite.git && cd php-mtn-v12-iframe-ref1-sqlite && rmdir .git /S /Q && cd ..
+git clone https://github.com/winofsql/php-mtn-v13-iframe-ref2-sqlite.git && cd php-mtn-v13-iframe-ref2-sqlite && rmdir .git /S /Q && cd ..
+git clone https://github.com/winofsql/php-mtn-v14-pdf-print-sqlite.git && cd php-mtn-v14-pdf-print-sqlite && rmdir .git /S /Q && cd ..
+
