c180d47d02d096bd06d959a3f9b23bb96788f9fb
sworc <lightbox@nifty.com>
2023-12-12 13:26:54 +0900

    Add files via upload

---
+++ workspace/doc/windows/files/download-php-mtn-all.bat
@@ -0,0 +1,15 @@
+git clone https://github.com/winofsql/php-mtn-v01-update.git && cd php-mtn-v01-update && rmdir .git /S /Q && cd ..
+git clone https://github.com/winofsql/php-mtn-v02-update-model.git && cd php-mtn-v02-update-model && rmdir .git /S /Q && cd ..
+git clone https://github.com/winofsql/php-mtn-v03-update-insert.git && cd php-mtn-v03-update-insert && rmdir .git /S /Q && cd ..
+git clone https://github.com/winofsql/php-mtn-v04-communication.git && cd php-mtn-v04-communication && rmdir .git /S /Q && cd ..
+git clone https://github.com/winofsql/php-mtn-v05-enter-control.git && cd php-mtn-v05-enter-control && rmdir .git /S /Q && cd ..
+git clone https://github.com/winofsql/php-mtn-v06-combo-box.git && cd php-mtn-v06-combo-box && rmdir .git /S /Q && cd ..
+git clone https://github.com/winofsql/php-mtn-v07-html-validator.git && cd php-mtn-v07-html-validator && rmdir .git /S /Q && cd ..
+git clone https://github.com/winofsql/php-mtn-v08-sql-adjustment.git && cd php-mtn-v08-sql-adjustment && rmdir .git /S /Q && cd ..
+git clone https://github.com/winofsql/php-mtn-v09-type-date.git && cd php-mtn-v09-type-date && rmdir .git /S /Q && cd ..
+git clone https://github.com/winofsql/php-mtn-v10-check-entry.git && cd php-mtn-v10-check-entry && rmdir .git /S /Q && cd ..
+git clone https://github.com/winofsql/php-mtn-v11-edit-control.git && cd php-mtn-v11-edit-control && rmdir .git /S /Q && cd ..
+git clone https://github.com/winofsql/php-mtn-v12-iframe-ref1.git && cd php-mtn-v12-iframe-ref1 && rmdir .git /S /Q && cd ..
+git clone https://github.com/winofsql/php-mtn-v13-iframe-ref2.git && cd php-mtn-v13-iframe-ref2 && rmdir .git /S /Q && cd ..
+git clone https://github.com/winofsql/php-mtn-v14-pdf-print.git && cd php-mtn-v14-pdf-print && rmdir .git /S /Q && cd ..
+
---
+++ workspace/doc/windows/files/download-php-req-all.bat
@@ -0,0 +1,9 @@
+git clone https://github.com/winofsql/php-req-v01-basic.git && cd php-req-v01-basic && rmdir .git /S /Q && cd ..
+git clone https://github.com/winofsql/php-req-v02-sql-item-layout.git && cd php-req-v02-sql-item-layout && rmdir .git /S /Q && cd ..
+git clone https://github.com/winofsql/php-req-v03-csv-download.git && cd php-req-v03-csv-download && rmdir .git /S /Q && cd ..
+git clone https://github.com/winofsql/php-req-v04-table-scroll.git && cd php-req-v04-table-scroll && rmdir .git /S /Q && cd ..
+git clone https://github.com/winofsql/php-req-v05-row-update.git && cd php-req-v05-row-update && rmdir .git /S /Q && cd ..
+git clone https://github.com/winofsql/php-req-v06-row-update-td-input.git && cd php-req-v06-row-update-td-input && rmdir .git /S /Q && cd ..
+git clone https://github.com/winofsql/php-req-v07-row-update-checked.git && cd php-req-v07-row-update-checked && rmdir .git /S /Q && cd ..
+git clone https://github.com/winofsql/php-req-v08-row-update-selected.git && cd php-req-v08-row-update-selected && rmdir .git /S /Q && cd ..
+git clone https://github.com/winofsql/php-req-ajax-v01-table-scroll.git && cd php-req-ajax-v01-table-scroll && rmdir .git /S /Q && cd ..
