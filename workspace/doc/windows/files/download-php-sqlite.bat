1a9802987ae8b16c6241a6914d2cd424e4142329
sworc <lightbox@nifty.com>
2023-12-15 08:09:52 +0900

    Add files via upload

--- workspace/doc/windows/files/download-php-sqlite.bat
+++ workspace/doc/windows/files/download-php-sqlite.bat
@@ -4,16 +4,19 @@ cd php-sqlite
 
 mkdir board
 cd  board
+C:\Users\%USERNAME%\AppData\Roaming\Code\User\script\download.vbs https://github.com/winofsql/resource-winofsql/raw/main/sqlite3 bbs.sqlite3
 Call ..\..\workspace\doc\windows\files\download-php-board-sqlite-all.bat
 
 cd ..\
 mkdir mtn
 cd mtn
+C:\Users\%USERNAME%\AppData\Roaming\Code\User\script\download.vbs https://github.com/winofsql/resource-winofsql/raw/main/sqlite3 lightbox.sqlite3
 Call ..\..\workspace\doc\windows\files\download-php-mtn-sqlite-all.bat
 
 cd ..\
 mkdir req
 cd req
+C:\Users\%USERNAME%\AppData\Roaming\Code\User\script\download.vbs https://github.com/winofsql/resource-winofsql/raw/main/sqlite3 lightbox.sqlite3
 Call ..\..\workspace\doc\windows\files\download-php-req-sqlite-all.bat
 
 cd ..\
